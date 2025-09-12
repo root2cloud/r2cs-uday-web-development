6b3fff2f98f745dc8e9a885e4429105fc49504be
Udaykiran Gardas <gardasudaykiran2001@gmail.com>
2025-09-12 18:02:26 +0530

    Implemented advanced search and responsive property pages

--- rental_estate/controllers/main.py
+++ rental_estate/controllers/main.py
@@ -5,21 +5,31 @@ from odoo.http import request
 class RealEstateWebsite(http.Controller):
 
     @http.route(['/properties', '/properties/<string:ptype>'], type='http', auth='public', website=True)
-    def list_properties(self, ptype=None, **kwargs):
+    def list_properties(self, ptype=None, zip_code=None, property_type=None, **kwargs):
         valid_types = ['buy', 'rent', 'sale']
 
+        # Validate property type from URL or query param
         if ptype and ptype not in valid_types:
             return request.not_found()
 
+        # Determine filter property_type priority:
+        # ptype from URL (for specific pages) overrides property_type query param (for all properties)
+        filter_type = ptype or (property_type if property_type in valid_types else None)
+
         domain = [('is_published', '=', True)]
-        if ptype:
-            domain.append(('property_type', '=', ptype))
+        if filter_type:
+            domain.append(('property_type', '=', filter_type))
+
+        if zip_code:
+            domain.append(('zip_code', '=', zip_code))
 
         properties = request.env['product.template'].sudo().search(domain)
 
         return request.render('rental_estate.property_listing_page', {
             'properties': properties,
-            'property_type': ptype or 'all',
+            'property_type': filter_type or 'all',
+            'zip_code': zip_code or '',
+            'valid_types': valid_types,
         })
 
     @http.route(['/property/<string:ptype>/<int:property_id>'], type='http', auth='public', website=True)
