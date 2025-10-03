33fce89a85fe530ad11baec4b1390c12265f13a2
Udaykiran Gardas <gardasudaykiran2001@gmail.com>
2025-10-03 18:04:47 +0530

    redefined home page.

--- real_estate_management/__manifest__.py
+++ real_estate_management/__manifest__.py
@@ -5,20 +5,21 @@
     'category': 'Website',
     'summary': 'Module for managing real estate properties and website integration',
     'description': 'Manage real estate properties with interactive map, listings, and contact forms.',
-    'depends': ['base', 'base_geolocalize', 'web', 'website_sale', 'mail', 'base_setup'], 'data': [
-    # Security
-    'security/ir.model.access.csv',
+    'depends': ['base', 'base_geolocalize', 'web', 'website_sale', 'mail', 'base_setup'],
+    'data': [
+        # Security
+        'security/ir.model.access.csv',
 
-    # Views
-    'views/property_views.xml',
-    'views/property_category_views.xml',
-    'views/menu.xml',
+        # Views
+        'views/property_views.xml',
+        'views/property_category_views.xml',
+        'views/menu.xml',
 
-    # Qweb Templates
-    'views/qweb_templates/property_map_template.xml',
-    'views/qweb_templates/property_detail_page.xml'
+        # Qweb Templates
+        'views/qweb_templates/property_map_template.xml',
+        'views/qweb_templates/property_detail_page.xml'
 
-],
+    ],
     'assets': {
         'web.assets_frontend': [
             # 'real_estate_management/static/src/js/property_map.js',
